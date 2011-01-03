7629ccfb6cf0d005d2b62a9f7c5fabe991a605ae
tepelstreel <tepelstreel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-03 19:59:37 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/category-coloumn/trunk@328389 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ category_column/category_column.php
@@ -0,0 +1,257 @@
+<?php
+/*
+Plugin Name: Category Column
+Plugin URI: http://wasistlos.waldemarstoffel.com/plugins-fur-wordpress/category-column-plugin
+Description: The Category Column does simply, what the name says; it creates a widget, which you can drag to your sidebar and it will show excerpts of the posts of other categories than showed in the center-column. The plugin is tested with WP 3.0.3. It might work with elder versions too, but those will never be explicitly supported. The plugin has a couple of adjustable parameters.  You can choose the number of posts displayed, the offset on your homepage and whether or not a line is displayed between the posts. Further customization will be added in later versions. Starting from version 2.3, it has a multi widget.
+Version: 2.9
+Author: Waldemar Stoffel
+Author URI: http://www.waldemarstoffel.com
+License: GPL3
+*/
+
+/*  Copyright 2010  Waldemar Stoffel  (email : stoffel@atelier-fuenf.de)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+
+/* Stop direct call */
+
+if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) { die("Sorry, you don't have direct access to this page."); }
+
+
+// extending the widget class
+ 
+class Category_Column_Widget extends WP_Widget {
+ 
+ function Category_Column_Widget() {
+ parent::WP_Widget(false, $name = 'Category Column');
+ }
+ 
+ function form($instance) {
+ $title = esc_attr($instance['title']);
+ $postcount = esc_attr($instance['postcount']);
+ $offset = esc_attr($instance['offset']);
+ $homepage = esc_attr($instance['homepage']);
+ $list = esc_attr($instance['list']);
+ $wordcount = esc_attr($instance['wordcount']);
+ $words = esc_attr($instance['words']);
+ $line=esc_attr($instance['line']);
+ $line_color=esc_attr($instance['line_color']);
+ $style=esc_attr($instance['style']);
+ 
+ ?>
+ 
+<p>
+ <label for="<?php echo $this->get_field_id('title'); ?>">
+ <?php _e('Title:'); ?>
+ <input id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('list'); ?>">
+ <?php _e("To exclude certain categories or to show just a special category, simply write their ID's separated by comma (e.g. <strong>-5,2,4</strong> will show categories 2 and 4 and will exclude category 5):"); ?>
+ <input id="<?php echo $this->get_field_id('list'); ?>" name="<?php echo $this->get_field_name('list'); ?>" type="text" value="<?php echo $list; ?>" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('postcount'); ?>">
+ <?php _e('How many posts will be displayed in the sidebar:'); ?>
+ <input id="<?php echo $this->get_field_id('postcount'); ?>" name="<?php echo $this->get_field_name('postcount'); ?>" type="text" value="<?php echo $postcount; ?>" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('offset'); ?>">
+ <?php _e('Offset (how many posts are spared out in the beginning):'); ?>
+ <input id="<?php echo $this->get_field_id('offset'); ?>" name="<?php echo $this->get_field_name('offset'); ?>" type="text" value="<?php echo $offset; ?>" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('homepage'); ?>">
+ <?php _e('Check to have the offset only on your homepage:'); ?>
+ <input id="<?php echo $this->get_field_id('homepage'); ?>" name="<?php echo $this->get_field_name('homepage'); ?>" <?php if(!empty($homepage)) {echo "checked=\"checked\""; } ?> type="checkbox" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('wordcount'); ?>">
+ <?php _e('In case there is no excerpt defined, how many sentences are displayed:'); ?>
+ <input id="<?php echo $this->get_field_id('wordcount'); ?>" name="<?php echo $this->get_field_name('wordcount'); ?>" type="text" value="<?php echo $wordcount; ?>" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('words'); ?>">
+ <?php _e('Check to display words instead of sentences:'); ?>
+ <input id="<?php echo $this->get_field_id('words'); ?>" name="<?php echo $this->get_field_name('words'); ?>" <?php if(!empty($words)) {echo "checked=\"checked\""; } ?> type="checkbox" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('line'); ?>">
+ <?php _e('If you want a line between the posts, this is the height in px (if not wanting a line, leave emtpy):'); ?>
+ <input id="<?php echo $this->get_field_id('line'); ?>" name="<?php echo $this->get_field_name('line'); ?>" type="text" value="<?php echo $line; ?>" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('line_color'); ?>">
+ <?php _e('The color of the line (e.g. #cccccc):'); ?>
+ <input id="<?php echo $this->get_field_id('line_color'); ?>" name="<?php echo $this->get_field_name('line_color'); ?>" type="text" value="<?php echo $line_color; ?>" />
+ </label>
+</p>
+<p>
+ <label for="<?php echo $this->get_field_id('style'); ?>">
+ <?php _e('Here you can finally style the widget. Simply type something like<br /><strong>border-left: 1px dashed;<br />border-color: #000000;</strong><br />to get just a dashed black line on the left. If you leave that section empty, your theme will style the widget'); ?>
+ <input id="<?php echo $this->get_field_id('style'); ?>" name="<?php echo $this->get_field_name('style'); ?>" type="textarea" value="<?php echo $style; ?>" />
+ </label>
+</p>
+<?php
+ }
+ 
+ function update($new_instance, $old_instance) {
+ 
+ $instance = $old_instance;
+ 
+ $instance['title'] = strip_tags($new_instance['title']);
+ $instance['postcount'] = strip_tags($new_instance['postcount']);
+ $instance['offset'] = strip_tags($new_instance['offset']);
+ $instance['homepage'] = strip_tags($new_instance['homepage']);
+ $instance['list'] = strip_tags($new_instance['list']); 
+ $instance['wordcount'] = strip_tags($new_instance['wordcount']);
+ $instance['words'] = strip_tags($new_instance['words']);
+ $instance['line'] = strip_tags($new_instance['line']);
+ $instance['line_color'] = strip_tags($new_instance['line_color']);
+ $instance['style'] = strip_tags($new_instance['style']);
+
+ return $instance;
+}
+ 
+ function widget($args, $instance) {
+ extract( $args );
+ $title = apply_filters('widget_title', $instance['title']);
+ 
+if (empty($instance['style'])) {
+	$cc_before_widget=$before_widget;
+	$cc_after_widget=$after_widget;
+}
+
+else {
+	$cc_before_widget="<div style=\"".$instance['style']."\">";
+	$cc_after_widget="</div>";
+}
+
+ echo $cc_before_widget;
+ 
+ if ( $title ) {
+	 echo $before_title . $title . $after_title;
+ }
+ 
+/* This is the actual function of the plugin, it fills the sidebar with the customized excerpts */
+
+$i=1;
+
+$cc_setup="numberposts=".$instance['postcount'];
+
+if (is_home() || empty($instance['homepage'])) {
+	$cc_setup.='&offset='.$instance['offset'];
+}
+
+if (is_category() && !$instance['list']) {
+	$cc_cat=get_query_var('cat');
+}
+
+if ($instance['list'] || $cc_cat) {
+	$cc_setup.='&cat='.$instance['list'].',-'.$cc_cat;
+}
+
+
+ global $post;
+ $myposts = get_posts($cc_setup);
+ foreach($myposts as $post) :
+ 
+   setup_postdata($post);
+   
+   if (has_post_thumbnail()) {
+	   
+/* If there is a picture, show thumbnail and headline */
+	   
+	   ?>
+       <a href="<?php the_permalink(); ?>">
+       <?php the_post_thumbnail(); ?>
+       </a><p><a href="<?php the_permalink(); ?>">
+       <?php the_title(); ?>
+       </a></p>
+       <?php 
+
+}
+	   
+	   else {
+		   
+/* If there is no picture, show headline and excerpt of the post */
+		   
+	
+	?>
+    <p><a href="<?php the_permalink(); ?>">
+    <?php the_title(); ?>
+    </a></p>
+    <?php
+
+
+	$cc_excerpt=$post->post_excerpt;
+	
+/* in case the excerpt is not definded by theme or anything else, the first x sentences of the content are given */
+	
+	if (empty($cc_excerpt)) {
+		
+		$cc_text=explode('[/caption]', get_the_content());
+		
+		if ($instance['words']) {
+			
+			$cc_short=array_slice(explode(" ", end ($cc_text)), 0, $instance['wordcount']);
+			
+			$cc_excerpt=implode(" ", $cc_short)."...";
+			
+		}
+		
+		else {
+			
+			$cc_short=array_slice(explode(". ", end ($cc_text)), 0, $instance['wordcount']-1);
+			
+			$cc_excerpt=implode(". ", $cc_short).".";
+			
+		}
+	
+	}
+	
+	echo "<p>".$cc_excerpt."</p>";
+	
+	   }
+	   
+	if (!empty($instance['line']) && $i <  $instance['postcount']) {
+		
+		echo "<hr style=\"color: ".$instance['line_color']."; background-color: ".$instance['line_color']."; height: ".$instance['line']."px;\" />";
+		
+		$i++;
+		
+		}
+	
+	endforeach;
+
+ 
+ echo $cc_after_widget;
+ 
+ }
+ 
+}
+
+add_action('widgets_init', create_function('', 'return register_widget("Category_Column_Widget");'));
+?>
---
+++ readme.txt
@@ -0,0 +1,46 @@
+=== Category Column ===
+Contributors: tepelstreel
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TQ9M9VJMAWA3Q
+Tags: column, sidebar, widget, category
+Requires at least: 2.7
+Tested up to: 3.0.4
+Stable tag: 2.9
+
+The Category Column does simply, what the name says; it will show excerpts of the latest posts in your sidebar.
+
+== Description ==
+
+The Category Column is mainly designed to give your blog a bit more of a newspaper behaviour. E.g. The plugin shows the latest posts from all categories with an offset of three posts (which are in the main column) on our homepage.
+
+If there is a post thumbnail, it will be displayed above the headline of the post. No further text will appear. If there is no thumbnail, only the headline and the excerpt of the post will be shown. When the plugin can detect neither the thumbnail nor the excerpt of a post, it will display just the first couple of sentences or (words) of a post.
+
+Thr Category Column was designed and tested in WP 3.0.3 and 3.0.4. It should work wih versions down to 2.7 but was never tested.
+
+== Installation ==
+
+1. Upload the `category_column` folder to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Place and customize your widgets
+4. Ready
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+== Changelog ==
+
+=2.9=
+
+The Category Column is a fully customizable multi widget now
+
+=2.1=
+
+Plugin cleans the options database after itself on deinstallation
+
+=2.0=
+
+Customizable for a bit and initial release
+
+=1.0=
+
+Basic plugin for personal use
