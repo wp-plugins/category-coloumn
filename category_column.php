6698429dee9d25db3d99d0cb17660024df449f94
tepelstreel <tepelstreel@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-25 21:00:59 +0000

    Category Column 4.2
    git-svn-id: https://plugins.svn.wordpress.org/category-coloumn/tags/4.2@902879 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- category_column.php
+++ category_column.php
@@ -3,7 +3,7 @@
 Plugin Name: Category Column
 Plugin URI: http://wasistlos.waldemarstoffel.com/plugins-fur-wordpress/category-column-plugin
 Description: The Category Column does simply, what the name says; it creates a widget, which you can drag to your sidebar and it will show excerpts of the posts of other categories than showed in the center-column. The plugin is tested with WP up to version 3.9. It might work with versions down to 2.7, but that will never be explicitly supported. The plugin has fully adjustable widgets. You can choose the number of posts displayed, the offset (only on your homepage or always) and whether or not a line is displayed between the posts. And much more.
-Version: 3.9
+Version: 4.2
 Author: Waldemar Stoffel
 Author URI: http://www.waldemarstoffel.com
 License: GPL3
@@ -30,48 +30,49 @@ Text Domain: category_column
 
 /* Stop direct call */
 
-if(preg_match('#' . basename(__FILE__) . '#', $_SERVER['PHP_SELF'])) die('Sorry, you don&#39;t have direct access to this page.');
+defined('ABSPATH') OR exit;
 
-define( 'CC_PATH', plugin_dir_path(__FILE__) );
+if (!defined('CC_PATH')) define( 'CC_PATH', plugin_dir_path(__FILE__) );
+if (!defined('CC_BASE')) define( 'CC_BASE', plugin_basename(__FILE__) );
 
+# loading the framework
 if (!class_exists('A5_Image')) require_once CC_PATH.'class-lib/A5_ImageClass.php';
 if (!class_exists('A5_Excerpt')) require_once CC_PATH.'class-lib/A5_ExcerptClass.php';
 if (!class_exists('A5_FormField')) require_once CC_PATH.'class-lib/A5_FormFieldClass.php';
-if (!class_exists('Category_Column_Widget')) require_once CC_PATH.'class-lib/CC_WidgetClass.php';
-if (!class_exists('A5_DynamicCSS')) :
+if (!class_exists('A5_OptionPage')) require_once CC_PATH.'class-lib/A5_OptionPageClass.php';
+if (!class_exists('A5_DynamicFiles')) require_once CC_PATH.'class-lib/A5_DynamicFileClass.php';
 
-	require_once CC_PATH.'class-lib/A5_DynamicCSSClass.php';
-	
-	$dynamic_css = new A5_DynamicCSS;
-	
-endif;
+#loading plugin specific classes
+if (!class_exists('CC_Admin')) require_once CC_PATH.'class-lib/CC_AdminClass.php';
+if (!class_exists('CC_DynamicCSS')) require_once CC_PATH.'class-lib/CC_DynamicCSSClass.php';
+if (!class_exists('Category_Column_Widget')) require_once CC_PATH.'class-lib/CC_WidgetClass.php';
 
 class CategoryColumn {
 
-const language_file = 'category_column';
+	const language_file = 'category_column';
+
+	private static $options;
 
 	function __construct() {
 		
 		// import laguage files
 	
-		load_plugin_textdomain(self::language_file, false , basename(dirname(__FILE__)).'/languages');	
+		load_plugin_textdomain(self::language_file, false , basename(dirname(__FILE__)).'/languages');
 		
-		add_action('admin_enqueue_scripts', array($this, 'enqueue_scripts'));
-		add_filter('plugin_row_meta', array($this, 'register_links'),10,2);
+		add_action('admin_enqueue_scripts', array(&$this, 'enqueue_scripts'));
 		
-		register_activation_hook(  __FILE__, array($this, 'set_options') );
-		register_deactivation_hook(  __FILE__, array($this, 'unset_options') );
+		add_filter('plugin_row_meta', array(&$this, 'register_links'), 10, 2);	
+		add_filter( 'plugin_action_links', array(&$this, 'plugin_action_links'), 10, 2 );
+				
+		register_activation_hook(  __FILE__, array(&$this, '_install') );
+		register_deactivation_hook(  __FILE__, array(&$this, '_uninstall') );
 		
-		$eol = "\r\n";
-		$tab = "\t";
+		self::$options = get_option('cc_options');
 		
-		A5_DynamicCSS::$styles .= $eol.'/* CSS portion of the Category Column */'.$eol.$eol;
+		if (isset(self::$options['tags'])) $this->update_plugin_options();
 		
-		A5_DynamicCSS::$styles .= 'p[id^="cc_byline"] {'.$eol.$tab.'font-size: 0.9em;'.$eol.'}'.$eol;
-		
-		A5_DynamicCSS::$styles .= 'p[id^="cc_byline"] a {'.$eol.$tab.'text-decoration: none !important;'.$eol.$tab.'font-weight: normal !important;'.$eol.'}'.$eol;
-		
-		A5_DynamicCSS::$styles.='div[id^="category_column_widget"].widget_category_column_widget img {'.$eol.$tab.'height: auto;'.$eol.$tab.'max-width: 100%;'.$eol.'}'.$eol;
+		$CC_DynamicCSS = new CC_DynamicCSS;
+		$CC_Admin = new CC_Admin;
 		
 	}
 
@@ -79,7 +80,7 @@ const language_file = 'category_column';
 	
 	function enqueue_scripts($hook) {
 		
-		if ($hook != 'widgets.php') return;
+		if ($hook != 'settings_page_category-column-settings' && $hook != 'widgets.php') return;
 		
 		wp_register_script('ta-expander-script', plugins_url('ta-expander.js', __FILE__), array('jquery'), '2.0', true);
 		wp_enqueue_script('ta-expander-script');
@@ -90,23 +91,30 @@ const language_file = 'category_column';
 	
 	function register_links($links, $file) {
 		
-		$base = plugin_basename(__FILE__);
-		if ($file == $base) {
+		if ($file == CC_BASE) {
 			$links[] = '<a href="http://wordpress.org/extend/plugins/category-coloumn/faq/" target="_blank">'.__('FAQ', self::language_file).'</a>';
 			$links[] = '<a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=TQ9M9VJMAWA3Q" target="_blank">'.__('Donate', self::language_file).'</a>';
 		}
 		
+		return $links;
+		
+	}
+		
+	function plugin_action_links( $links, $file ) {
+		
+		if ($file == CC_BASE) array_unshift($links, '<a href="'.admin_url( 'options-general.php?page=category-column-settings' ).'">'.__('Settings', self::language_file).'</a>');
+	
 		return $links;
 	
 	}
 	
 	// Creating default options on activation
 	
-	function set_options() {
+	function _install() {
 		
 		$default = array(
-			'tags' => array(),
-			'sizes' => array()
+			'cache' => array(),
+			'inline' => false
 		);
 		
 		add_option('cc_options', $default);
@@ -115,10 +123,24 @@ const language_file = 'category_column';
 	
 	// Cleaning on deactivation
 	
-	function unset_options() {
+	function _uninstall() {
 		
 		delete_option('cc_options');
 		
+	}
+	
+	// updating options in case they are outdated
+	
+	function update_plugin_options() {	
+		
+			self::$options['cache'] = array();
+			
+			self::$options['inline'] = false;
+			
+			unset(self::$options['tags'], self::$options['sizes']);
+			
+			update_option('cc_options', self::$options);
+	
 	}
 
 } // end of class
--- class-lib/CC_WidgetClass.php
+++ class-lib/CC_WidgetClass.php
@@ -12,6 +12,8 @@
 class Category_Column_Widget extends WP_Widget {
 	
 const language_file = 'category_column';
+
+private static $options;
  
 function Category_Column_Widget() {
 
@@ -19,6 +21,8 @@ function Category_Column_Widget() {
 	$control_opts = array( 'width' => 400 );
 	
 	parent::WP_Widget(false, $name = 'Category Column', $widget_opts, $control_opts);
+	
+	self::$options = get_option('cc_options');
 
 }
  
@@ -36,12 +40,13 @@ function form($instance) {
 		'showcat_txt' => NULL,
 		'wordcount' => 3,
 		'linespace' => NULL,
-		'width' => NULL,
+		'width' => get_option('thumbnail_size_w'),
 		'words' => NULL,
 		'line' => 1,
 		'line_color' => '#dddddd',
 		'style' => NULL,
 		'h' => 3,
+		'imgborder' => NULL,
 		'headonly' => NULL
 	);
 	
@@ -63,6 +68,7 @@ function form($instance) {
 	$style=esc_attr($instance['style']);
 	$h = esc_attr($instance['h']);
 	$headonly = esc_attr($instance['headonly']);
+	$imgborder=esc_attr($instance['imgborder']);
 	
 	$base_id = 'widget-'.$this->id_base.'-'.$this->number.'-';
 	$base_name = 'widget-'.$this->id_base.'['.$this->number.']';
@@ -77,6 +83,7 @@ function form($instance) {
 	a5_number_field($base_id.'offset', $base_name.'[offset]', $offset, __('Offset (how many posts are spared out in the beginning):', self::language_file), array('space' => true, 'size' => 4, 'step' => 1));
 	a5_checkbox($base_id.'home', $base_name.'[home]', $home, __('Check to have the offset only on your homepage.', self::language_file), array('space' => true));
 	a5_number_field($base_id.'width', $base_name.'[width]', $width, __('Width of the thumbnail (in px):', self::language_file), array('space' => true, 'size' => 4, 'step' => 1));
+	a5_text_field($base_id.'imgborder', $base_name.'[imgborder]', $imgborder, sprintf(__('If wanting a border around the image, write the style here. %s would make it a black border, 1px wide.', self::language_file), '<strong>1px solid #000000</strong>'), array('space' => true, 'class' => 'widefat'));
 	a5_select($base_id.'h', $base_name.'[h]', $headings, $h, __('Weight of the Post Title:', self::language_file), false, array('space' => true));
 	a5_checkbox($base_id.'headonly', $base_name.'[headonly]', $headonly, __('Check to display only the headline of the post.', self::language_file), array('space' => true));
 	a5_number_field($base_id.'wordcount', $base_name.'[wordcount]', $wordcount, __('In case there is no excerpt defined, how many sentences are displayed:', self::language_file), array('space' => true, 'size' => 4, 'step' => 1));
@@ -84,8 +91,8 @@ function form($instance) {
 	a5_checkbox($base_id.'linespace', $base_name.'[linespace]', $linespace, __('Check to have each sentense in a new line.', self::language_file), array('space' => true));
 	a5_number_field($base_id.'line', $base_name.'[line]', $line, __('If you want a line between the posts, this is the height in px (if not wanting a line, leave emtpy):', self::language_file), array('space' => true, 'size' => 4, 'step' => 1));
 	a5_color_field($base_id.'line_color', $base_name.'[line_color]', $line_color, __('The color of the line (e.g. #cccccc):', self::language_file), array('space' => true, 'size' => 13));
-	a5_textarea($base_id.'style', $base_name.'[style]', $style, sprintf(__('Here you can finally style the widget. Simply type something like%1$s%2$sborder-left: 1px dashed;%2$sborder-color: #000000;%3$s%2$sto get just a dashed black line on the left. If you leave that section empty, your theme will style the widget.', self::language_file), '<strong>', '<br />', '</strong>'), array('space' => true, 'class' => 'widefat', 'style' => 'height: 60px;'));
-	a5_resize_textarea(array($base_id.'style'), true);
+	if (empty(self::$options['css'])) a5_textarea($base_id.'style', $base_name.'[style]', $style, sprintf(__('Here you can finally style the widget. Simply type something like%1$s%2$sborder-left: 1px dashed;%2$sborder-color: #000000;%3$s%2$sto get just a dashed black line on the left. If you leave that section empty, your theme will style the widget.', self::language_file), '<strong>', '<br />', '</strong>'), array('space' => true, 'class' => 'widefat', 'style' => 'height: 60px;'));
+	a5_resize_textarea(array($base_id.'style'));
 
 } // form
 
@@ -98,18 +105,18 @@ function update($new_instance, $old_instance) {
 	$instance['offset'] = strip_tags($new_instance['offset']);
 	$instance['home'] = strip_tags($new_instance['home']);
 	$instance['list'] = strip_tags($new_instance['list']);
-	$instance['showcat'] = strip_tags($new_instance['showcat']);
+	$instance['showcat'] = @$new_instance['showcat'];
 	$instance['showcat_txt'] = strip_tags($new_instance['showcat_txt']); 	
 	$instance['wordcount'] = strip_tags($new_instance['wordcount']);
 	$instance['width'] = strip_tags($new_instance['width']);
-	$instance['words'] = strip_tags($new_instance['words']);
-	$instance['adsense'] = strip_tags($new_instance['adsense']);
-	$instance['linespace'] = strip_tags($new_instance['linespace']);
+	$instance['words'] = @$new_instance['words'];
+	$instance['linespace'] = @$new_instance['linespace'];
 	$instance['line'] = strip_tags($new_instance['line']);
 	$instance['line_color'] = strip_tags($new_instance['line_color']);
 	$instance['style'] = strip_tags($new_instance['style']);
 	$instance['h'] = strip_tags($new_instance['h']);
-	$instance['headonly'] = strip_tags($new_instance['headonly']);
+	$instance['headonly'] = @$new_instance['headonly'];
+	$instance['imgborder'] = strip_tags($new_instance['imgborder']);
 	
 	return $instance;
 
@@ -123,21 +130,15 @@ function widget($args, $instance) {
 	
 	$title = apply_filters('widget_title', $instance['title']);
 	
-	if (empty($instance['style'])) :
-	
-		$cc_before_widget=$before_widget;
-		$cc_after_widget=$after_widget;
-	
-	else :
-	
-		$cc_style=str_replace(array("\r\n", "\n", "\r"), '', $instance['style']);
+	if (!empty($instance['style'])) :
+			
+		$style=str_replace(array("\r\n", "\n", "\r"), '', $instance['style']);
 		
-		$cc_before_widget="<div id=\"".$widget_id."\" style=\"".$cc_style."\">";
-		$cc_after_widget="</div>";
+		$before_widget = str_replace('>', 'style="'.$style.'">', $before_widget);
 	
 	endif;
 	
-	echo $cc_before_widget;
+	echo $before_widget;
 	
 	if ( $title ) echo $before_title . $title . $after_title;
  
@@ -145,33 +146,31 @@ function widget($args, $instance) {
 	
 	$i=1;
 	
-	$cc_setup="posts_per_page=".$instance['postcount'];
-	
-	if (is_home() || empty($instance['home'])) :
+	$cc_setup['posts_per_page'] = $instance['postcount'];
 		
 		global $wp_query;
 		
-		$cc_page = $wp_query->get( 'paged' );
+		if (is_category() || is_home() || empty($instance['home'])) :
+			
+			$cc_page = $wp_query->get( 'paged' );
+			
+			$cc_numberposts = $wp_query->get( 'posts_per_page' );
+			
+			$cc_offset = (empty($cc_page)) ? $cc_offset=$instance['offset'] : $cc_offset=(($cc_page-1)*$cc_numberposts)+$instance['offset'];
+			
+			$cc_setup['offset'] = $cc_offset;
 		
-		$cc_numberposts = $wp_query->get( 'posts_per_page' );
+		endif;
 		
-		$cc_offset = (empty($cc_page)) ? $cc_offset=$instance['offset'] : $cc_offset=(($cc_page-1)*$cc_numberposts)+$instance['offset'];
+		$cc_cat = (is_category()) ? ',-'.get_query_var('cat') : '';
 		
-		$cc_setup.='&offset='.$cc_offset;
-	
-	endif;
-	
-	if (is_category() && !$instance['list']) $cc_cat=get_query_var('cat');
-	
-	if ($instance['list'] || isset($cc_cat)) $cc_setup.='&cat='.$instance['list'].',-'.$cc_cat;
-	
-	if (is_single()) :
+		if ($instance['list'] || !empty($cc_cat)) $cc_setup['cat'] = $instance['list'].$cc_cat;
 		
-		global $wp_query;
+		if (is_single()) :
+			
+			$cc_setup['post__not_in'] = array($wp_query->get_queried_object_id()); 
 		
-		$cc_setup.='&exclude='.$wp_query->get_queried_object_id();
-		
-	endif;
+		endif;
 	
 	global $post;
 	
@@ -195,7 +194,7 @@ function widget($args, $instance) {
 			
 		endif;
 	
-		$cc_tags = A5_Image::tags($post, 'cc_options', self::language_file);
+		$cc_tags = A5_Image::tags(self::language_file);
 		
 		$cc_image_alt = $cc_tags['image_alt'];
 		$cc_image_title = $cc_tags['image_title'];
@@ -207,74 +206,29 @@ function widget($args, $instance) {
 		
 		if (empty($instance['headonly'])) :
 			
-			$default = A5_Image::get_default($instance['width']);
+			$cc_imgborder = (!empty($instance['imgborder'])) ? ' style="border: '.$instance['imgborder'].';"' : '';
 			
-			if (!has_post_thumbnail()) :
-				
-					$args = array (
-						'content' => $post->post_content,
-						'width' => $default[0],
-						'height' => $default[1], 
-						'option' => 'acc_options'
-					);	
-				   
-					$cc_image_info = A5_Image::thumbnail($args);
+			$id = get_the_ID();
 					
-					$cc_thumb = $cc_image_info['thumb'];
+			$args = array (
+				'id' => $id,
+				'option' => 'cc_options',
+				'width' => $instance['width']
+			);
 					
-					$cc_width = $cc_image_info['thumb_width'];
+			$cc_image_info = A5_Image::thumbnail($args);
+					
+			$cc_thumb = $cc_image_info[0];
 			
-					$cc_height = $cc_image_info['thumb_height'];
+			$cc_width = $cc_image_info[1];
+	
+			$cc_height = ($cc_image_info[2]) ? 'height="'.$cc_image_info[2].'"' : '';
 					
-					if ($cc_thumb) :
+			if ($cc_thumb) $cc_img = '<img title="'.$cc_image_title.'" src="'.$cc_thumb.'" alt="'.$cc_image_alt.'" class="wp-post-image" width="'.$cc_width.'" '.$cc_height.$cc_imgborder.' />';
+			
+		endif;
 					
-						if ($cc_width) $cc_img = '<img title="'.$cc_image_title.'" src="'.$cc_thumb.'" alt="'.$cc_image_alt.'" class="wp-post-image" width="'.$cc_width.'" height="'.$cc_height.'" />';
-							
-						else $cc_img = '<img title="'.$cc_image_title.'" src="'.$cc_thumb.'" alt="'.$cc_image_alt.'" class="wp-post-image" style="maxwidth: '.$width.'; maxheight: '.$height.';" />';
-						
-					endif;
-					
-				else :
-				
-					$img_info = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large');
-						
-					if (!$img_info):
-					
-						$src = get_the_post_thumbnail();
-					
-						$img = preg_match_all('/<\s*img[^>]+src\s*=\s*["\']?([^\s"\']+)["\']?[\s\/>]+/', $src, $matches);
-						
-						$img_info[0] = $matches[1][0];
-						
-						$img_size = A5_Image::get_size($img_info[0]);
-						
-						$img_info[1] = $img_size['width'];
-						
-						$img_info[2] = $img_size['height'];
-						
-					endif;
-					
-					$args = array (
-						'ratio' => $img_info[1]/$img_info[2],
-						'thumb_width' => $img_info[1],
-						'thumb_height' => $img_info[2],
-						'width' => $default[0],
-						'height' => $default[1]
-					);
-					
-					$img_size = A5_Image::count_size($args);
-					
-					$atts = array('title' => $cc_image_title, 'alt' => $cc_image_alt);
-					
-					$size = array($img_size['width'], $img_size['height']);
-				
-					$cc_img = get_the_post_thumbnail($post->ID, $size, $atts);
-					
-				endif;
-				
-			endif;
-					
-		if (isset($cc_img)) :
+		if (!empty($cc_img)) :
 			
 			echo '<a href="'.get_permalink().'">'.$cc_img.'</a>'.$eol.'<div style="clear: both;"></div>'.$eol.$cc_headline;
 	
@@ -323,7 +277,7 @@ function widget($args, $instance) {
 	wp_reset_query();
 	wp_reset_postdata();
 	
-	echo $cc_after_widget;
+	echo $after_widget;
 
 }
  
